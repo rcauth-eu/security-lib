669c95841d491e10da0505ffe341b7ed613eb47e
ttomttom <tamasb@nikhef.nl>
2016-01-19 15:10:29 +0100

    removed byte array proxy representation
    there is no need to have a duplicate variable with the same proxy in it

--- ncsa-security-delegation/ncsa-security-delegation-common/src/main/java/edu/uiuc/ncsa/security/delegation/token/MyX509Proxy.java
+++ ncsa-security-delegation/ncsa-security-delegation-common/src/main/java/edu/uiuc/ncsa/security/delegation/token/MyX509Proxy.java
@@ -15,13 +15,10 @@ import edu.uiuc.ncsa.security.util.pkcs.ProxyUtil;
 public class MyX509Proxy extends MyX509Certificates {
 
     protected PrivateKey proxyKey;
-	
-    protected byte[] proxy;
     
     public MyX509Proxy(byte[] pemProxy) {
     	super(ProxyUtil.certificatesFromProxy(pemProxy));
 		this.proxyKey = ProxyUtil.keyFromProxy(pemProxy);
-    	this.proxy = pemProxy;
 	}
 
     public MyX509Proxy(MyX509Certificates certs, PrivateKey key) {
@@ -38,38 +35,29 @@ public class MyX509Proxy extends MyX509Certificates {
 		return proxyKey;
 	}
     
-    public byte[] getProxy() throws CertificateEncodingException {
-    	if (proxy == null || proxy.length == 0) {
-    		proxy = this.getX509CertificatesPEM().getBytes();
-    	} 
-		return proxy;
-	}
-
-    public String getX509ProxyPEM() {
-    	return new String(proxy);
+    public String getX509ProxyPEM() throws CertificateEncodingException {     	
+    	return getX509CertificatesPEM();	
     }
     
     @Override
     public String getX509CertificatesPEM() throws CertificateEncodingException {
 
-    	if (proxy != null && proxy.length != 0) {
-    		return new String(proxy);
-    	} else {
-            String pem = "";
-           
-            if ( x509Certificates.length > 0 ) {
-            	pem += CertUtil.toPEM(x509Certificates[0]) + "\n";
-            }
-            
-            if ( proxyKey != null ) {
-            	pem += KeyUtil.toPKCS8PEM(proxyKey) + "\n";
-            }
-            
-            for (int i = 1; i < x509Certificates.length; ++i){
-                pem += CertUtil.toPEM(x509Certificates[i]) + "\n";
-            }
-            return pem;
-    	}
+
+        String pem = "";
+       
+        if ( x509Certificates.length > 0 ) {
+        	pem += CertUtil.toPEM(x509Certificates[0]) + "\n";
+        }
+        
+        if ( proxyKey != null ) {
+        	pem += KeyUtil.toPKCS8PEM(proxyKey) + "\n";
+        }
+        
+        for (int i = 1; i < x509Certificates.length; ++i){
+            pem += CertUtil.toPEM(x509Certificates[i]) + "\n";
+        }
+        return pem;
+	
     }
     
 }
